3a0bcf03f04f00adad037f3948f6fa4576679f90
klhaug <kristian@snougle.no>
2024-09-11 06:49:50 +0200

    starting project and building 2 components

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Tilt from 'react-parallax-tilt'
+import "./Logo.css"
+import brain from "./icons8-brain-64.png"
+
+const Logo = () => {
+    return (
+
+      <div className="ma4 mt0 ">
+            <Tilt className="Tilt br3 shadow-2" style={{width: "125px"}}>
+                <div className="Tilt-inner pa3">
+                    <img style={{paddingTop: "5px"}} src={brain} alt="logo"/>
+                </div>
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Tilt from 'react-parallax-tilt';
+import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
