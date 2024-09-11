a216836ca422c6ae6bdd15f0a53d4cbb64be1587
klhaug <kristian@snougle.no>
2024-09-11 07:11:48 +0200

    more components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo';
+import ImageLinkForm from './components/imagelinkform/ImageLinkForm';
+import Rank from './components/rank/Rank';
+
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
-      {/* {
+      <Rank />
       <ImageLinkForm />
-      <FaceRecognition />} */}
+      {
+      // <FaceRecognition />
+            }
     </div>
   );
 }
