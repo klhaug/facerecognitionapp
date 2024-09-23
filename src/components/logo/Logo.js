ce60478ff0de821372de31aa354b3d03a9d36234
klhaug <kristian@snougle.no>
2024-09-23 06:38:38 +0200

    fixing logo

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -7,7 +7,7 @@ const Logo = () => {
     return (
 
       <div className="ma4 mt0 ">
-            <Tilt className="Tilt br3 shadow-2" style={{width: "125px"}}>
+            <Tilt className="Tilt br4 shadow-2" style={{width: "100px"}}>
                 <div className="Tilt-inner pa3">
                     <img style={{paddingTop: "5px"}} src={brain} alt="logo"/>
                 </div>
